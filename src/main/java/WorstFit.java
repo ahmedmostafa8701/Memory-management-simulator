338fd5fa3749dc2f9de86ae11fc794d90fa9bcef
SAMY-MOHSEN <engsamy.m33@gmail.com>
2022-12-16 17:40:14 +0200

    Adding worst-fit policy

--- src/main/java/WorstFit.java
+++ src/main/java/WorstFit.java
@@ -3,6 +3,18 @@ import java.util.List;
 public class WorstFit implements Policy{
     @Override
     public Partition addToStorage(Process process, List<Partition> partitions) {
+        int idx = 0;
+        int size = -1;
+        for(int i = 0; i < partitions.size(); i++){
+            if(partitions.get(i).getSize() > size && partitions.get(i).getProcess() == null){
+                size = partitions.get(i).getSize();
+                idx = i;
+            }
+        }
+        if(idx != 0){
+            partitions.get(idx).setProcess(process);
+            return partitions.get(idx);
+        }
         return null;
     }
 }
